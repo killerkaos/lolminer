5b698eb9aa4c58f17f0a1ebf4523a1f56ac77e41
killerkaos <gesthal@gmail.com>
2024-01-15 16:13:40 -0500

    Update init-bash.sh

--- root/init-bash.sh
+++ root/init-bash.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "hi this is a line text" > /proc/1/fd/1
+echo "Conda init bash " > /proc/1/fd/1
+
+conda init bash
+
+echo "Conda activate " > /proc/1/fd/1
+
+conda activate vicuna
+
+echo "Conda install dependencies " > /proc/1/fd/1
+
+conda install pytorch torchvision torchaudio pytorch-cuda=11.7 cuda-toolkit -c 'nvidia/label/cuda-11.7.0' -c pytorch -c nvidia -y > /proc/1/fd/1
