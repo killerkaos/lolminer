abbb3af706258bd9b04efd7bea365a9fd874a2a4
killerkaos <gesthal@gmail.com>
2024-01-16 22:48:36 -0500

    Update entrypoint.sh

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -2,33 +2,33 @@
 conda init bash
 source ~/.bashrc
 
+if [ ! -d "/root/miniconda/envs/vicuna" ]; then
 echo "=========================================" > /proc/1/fd/1
 echo "CONDA CREATE vicuna" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 conda create -n vicuna python=3.9 -y > /proc/1/fd/1
-
 echo "=========================================" > /proc/1/fd/1
 echo "CONDA ACTIVATE vicuna" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 conda activate vicuna
-
 echo "=========================================" > /proc/1/fd/1
 echo "INSTALLING DEPENDENCIES: Torch, cudatoolkit & protobuf (WILL TAKE A LONG TIME)" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117 > /proc/1/fd/1
 conda install -c conda-forge cudatoolkit-dev -y > /proc/1/fd/1
 pip install protobuf==3.20 > /proc/1/fd/1
-
 echo "=========================================" > /proc/1/fd/1
 echo "DEPENDENCIES ALL INSTALLED" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
+fi
 
 echo "=========================================" > /proc/1/fd/1
 echo "INSTALLING REQUIREMENT for Fastchat" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-cd /root/Fastchat
-pip install -r requirements.txt -y > /proc/1/fd/1
+cd /root/FastChat
+pip install -r requirements.txt > /proc/1/fd/1
 
+if [ ! -d "/root/FastChat/repositories/GPTQ-for-LLaMa" ]; then
 echo "=========================================" > /proc/1/fd/1
 echo "CREATING REPOSITORIES & DOWNLOADING GPTQ for LLaMa" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
@@ -36,6 +36,7 @@ mkdir repositories
 cd repositories
 git clone https://github.com/oobabooga/GPTQ-for-LLaMa.git -b cuda > /proc/1/fd/1
 cd GPTQ-for-LLaMa
+fi
 
 echo "=========================================" > /proc/1/fd/1
 echo "COMPILING setup_cuda.py" > /proc/1/fd/1
