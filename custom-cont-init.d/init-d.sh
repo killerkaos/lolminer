4fe3a0d84ea56bdf9d6b4234d405adc51f3bf396
killerkaos <gesthal@gmail.com>
2024-01-20 18:33:09 -0500

    Update init-d.sh

--- custom-cont-init.d/init-d.sh
+++ custom-cont-init.d/init-d.sh
@@ -3,9 +3,22 @@ conda init bash
 source ~/.bashrc
 
 # make the folders
+
+if [ ! -d /config/miniconda ]; then
+mkdir -p \
+    /config/miniconda
+fi
+
+if [ ! -d /config/text-generation-webui ]; then
 mkdir -p \
     /config/text-generation-webui
-    /config/miniconda
+fi
 
+RUN wget https://raw.githubusercontent.com/killerkaos/vicuna/main/installer.sh -P
+bash installer.sh
+rm -f installer.sh
+
+RUN wget
+bash start.sh
 
 exec "$@"
