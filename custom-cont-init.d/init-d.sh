96fa270f7068f516c46b0e918c9666de70d51af1
killerkaos <gesthal@gmail.com>
2024-01-20 17:37:04 -0500

    Update init-d.sh

--- custom-cont-init.d/init-d.sh
+++ custom-cont-init.d/init-d.sh
@@ -43,10 +43,4 @@ echo "=========================================" > /proc/1/fd/1
 echo "FINISH INSTALLATION" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 
-echo "=========================================" > /proc/1/fd/1
-echo "EXECUTING PYTHON SERVER" > /proc/1/fd/1
-echo "=========================================" > /proc/1/fd/1
-cd /root/text-generation-webui
-python server.py > /proc/1/fd/1
-
 exec "$@"
