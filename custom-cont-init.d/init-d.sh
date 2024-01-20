230a1de393b7e1e70c7a4bca368fe0d3c0c9df75
killerkaos <gesthal@gmail.com>
2024-01-20 18:47:36 -0500

    Update init-d.sh

--- custom-cont-init.d/init-d.sh
+++ custom-cont-init.d/init-d.sh
@@ -8,20 +8,29 @@ echo "=========================================" > /proc/1/fd/1
 
 if [ ! -d /config/miniconda ]; then
 mkdir -p \
-    /config/miniconda
+    /config/miniconda/
+    cd /config/miniconda
+    ln -s /root/miniconda minicondalink > /proc/1/fd/1
 fi
 
 if [ ! -d /config/text-generation-webui ]; then
 mkdir -p \
-    /config/text-generation-webui
+    /config/text-generation-webui/
+    ln -s /root/text-generation-webui guilink > /proc/1/fd/1
 fi
 
+echo "=========================================" > /proc/1/fd/1
+echo "INSTALLER script" > /proc/1/fd/1
+echo "=========================================" > /proc/1/fd/1
 if [ ! -f /root/installed ]; then
 RUN wget https://raw.githubusercontent.com/killerkaos/vicuna/main/installer.sh
 bash installer.sh
 rm -f installer.sh
 fi
 
+echo "=========================================" > /proc/1/fd/1
+echo "STARTUP python server script" > /proc/1/fd/1
+echo "=========================================" > /proc/1/fd/1
 RUN wget https://raw.githubusercontent.com/killerkaos/vicuna/main/startup.sh
 bash startup.sh
 
