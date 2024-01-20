1cdd22c3f191940a9a833e5dbd2f240f813b15ac
killerkaos <gesthal@gmail.com>
2024-01-20 18:48:55 -0500

    Update init-d.sh

--- custom-cont-init.d/init-d.sh
+++ custom-cont-init.d/init-d.sh
@@ -7,15 +7,13 @@ echo "MAKING THE FOLDERS AND LINKING THEM" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 
 if [ ! -d /config/miniconda ]; then
-mkdir -p \
-    /config/miniconda/
+    mkdir -p /config/miniconda/
     cd /config/miniconda
     ln -s /root/miniconda minicondalink > /proc/1/fd/1
 fi
 
 if [ ! -d /config/text-generation-webui ]; then
-mkdir -p \
-    /config/text-generation-webui/
+    mkdir -p /config/text-generation-webui/
     ln -s /root/text-generation-webui guilink > /proc/1/fd/1
 fi
 
