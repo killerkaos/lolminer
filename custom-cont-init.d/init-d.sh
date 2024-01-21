c1cf5932322792e3544bf7da291e01eb8795e9ce
killerkaos <gesthal@gmail.com>
2024-01-20 19:15:44 -0500

    Update init-d.sh

--- custom-cont-init.d/init-d.sh
+++ custom-cont-init.d/init-d.sh
@@ -4,22 +4,32 @@ echo "MAKING THE FOLDERS AND LINKING THEM" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
 
 if [ ! -d /config/miniconda ]; then
-    ln -s /root/miniconda /config/miniconda > /proc/1/fd/1
+    echo "No miniconda folder detected, will create" > /proc/1/fd/1
+    ln -s /root/miniconda /config/miniconda
+    else
+    echo "Miniconda folder detected, skip" > /proc/1/fd/1
 fi
 
 if [ ! -d /config/text-generation-webui ]; then
+    echo "No text-generation-webui folder detected, will create" > /proc/1/fd/1
     ln -s /root/text-generation-webui /config/text-generation-webui > /proc/1/fd/1
+else
+    echo "Text-generation-webui folder detected, skip" > /proc/1/fd/1
 fi
 
+# Execute installer script
 if [ ! -f /root/installed ]; then
-echo "=========================================" > /proc/1/fd/1
-echo "INSTALLER script" > /proc/1/fd/1
-echo "=========================================" > /proc/1/fd/1
-wget https://raw.githubusercontent.com/killerkaos/vicuna/main/installer.sh
-bash installer.sh
-rm -f installer.sh
+    echo "=========================================" > /proc/1/fd/1
+    echo "INSTALLER script" > /proc/1/fd/1
+    echo "=========================================" > /proc/1/fd/1
+    wget https://raw.githubusercontent.com/killerkaos/vicuna/main/installer.sh
+    bash installer.sh
+    rm -f installer.sh
+else
+    echo "Vicuna installed, skip installer" > /proc/1/fd/1
 fi
 
+# Execute Startup script
 echo "=========================================" > /proc/1/fd/1
 echo "STARTUP python server script" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
