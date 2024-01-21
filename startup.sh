5aa07409e45ca110ba368ef61618eedf2fa1da47
killerkaos <gesthal@gmail.com>
2024-01-20 19:40:56 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,6 +3,14 @@ conda init bash > /proc/1/fd/1
 source ~/.bashrc
 conda activate vicuna
 
+if [ ! -f /root/text-generation-webui/models/anon8231489123_vicuna-13b-GPTQ-4bit-128g ]; then
+echo "=========================================" > /proc/1/fd/1
+echo "DOWNLOADING anon8231489123/vicuna-13b-GPTQ-4bit-128g" > /proc/1/fd/1
+echo "=========================================" > /proc/1/fd/1
+cd /root/text-generation-webui/
+python download-model.py anon8231489123/vicuna-13b-GPTQ-4bit-128g > /proc/1/fd/1
+fi
+
 echo "=========================================" > /proc/1/fd/1
 echo "EXECUTING PYTHON SERVER" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
