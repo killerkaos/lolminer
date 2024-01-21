cd9a92c9eee2a106f98038626afd9ea7b134aa50
killerkaos <gesthal@gmail.com>
2024-01-20 19:38:05 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 conda init bash > /proc/1/fd/1
 source ~/.bashrc
+conda activate vicuna
+
 echo "=========================================" > /proc/1/fd/1
 echo "EXECUTING PYTHON SERVER" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
