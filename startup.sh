2a93dc27492e8bd9eb54d151009e849a51bb1fd9
killerkaos <gesthal@gmail.com>
2024-01-20 19:19:08 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+conda init bash > /proc/1/fd/1
+source ~/.bashrc
 echo "=========================================" > /proc/1/fd/1
 echo "EXECUTING PYTHON SERVER" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
