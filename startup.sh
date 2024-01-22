754fccc36b60ef6fcc1fa13c5e74d87b6b8d727a
killerkaos <gesthal@gmail.com>
2024-01-21 23:07:15 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,5 +6,5 @@ conda activate vicuna
 echo "=========================================" > /proc/1/fd/1
 echo "EXECUTING PYTHON SERVER" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-cd /config/text-generation-webui
+cd /root/text-generation-webui
 python server.py --listen > /proc/1/fd/1
