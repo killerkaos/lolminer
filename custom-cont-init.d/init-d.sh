faa1391fd72dcfcba3bb750288570e879df8de8f
killerkaos <gesthal@gmail.com>
2024-01-21 22:34:40 -0500

    Update init-d.sh

--- custom-cont-init.d/init-d.sh
+++ custom-cont-init.d/init-d.sh
@@ -8,29 +8,11 @@ chown -R abc:abc /config
 echo "Done!"
 echo -e "-------------------------------------\n"
 
-echo "=========================================" > /proc/1/fd/1
-echo "MAKING THE FOLDERS AND LINKING THEM" > /proc/1/fd/1
-echo "=========================================" > /proc/1/fd/1
-
-if [ ! -d /config/miniconda ]; then
-    echo "No miniconda folder detected, will create" > /proc/1/fd/1
-    ln -s /root/miniconda /config/miniconda/
-    else
-    echo "Miniconda folder detected, skip" > /proc/1/fd/1
-fi
-
-if [ ! -d /config/text-generation-webui ]; then
-    echo "No text-generation-webui folder detected, will create" > /proc/1/fd/1
-    ln -s /root/text-generation-webui /config/text-generation-webui/ > /proc/1/fd/1
-else
-    echo "Text-generation-webui folder detected, skip" > /proc/1/fd/1
-fi
-
 # Execute installer script
 echo "=========================================" > /proc/1/fd/1
 echo "INSTALLER script" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-if [ ! -f /root/installed ]; then
+if [ ! -f /config/installed ]; then
     wget https://raw.githubusercontent.com/killerkaos/vicuna/main/installer.sh
     bash installer.sh
     rm -f installer.sh
