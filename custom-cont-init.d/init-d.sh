00281faeaa67bcf4f1f89fda7012e975f35c6c5e
killerkaos <gesthal@gmail.com>
2024-01-21 23:08:59 -0500

    Update init-d.sh

--- custom-cont-init.d/init-d.sh
+++ custom-cont-init.d/init-d.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-
-echo "-------------------------------------"
-# permissions
-echo "chown'ing directory to ensure correct permissions."
-chown -R abc:abc /home/abc
-chown -R abc:abc /config
-echo "Done!"
-echo -e "-------------------------------------\n"
-
 # Execute installer script
 echo "=========================================" > /proc/1/fd/1
 echo "INSTALLER script" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-if [ ! -f /config/installed ]; then
-    wget https://raw.githubusercontent.com/killerkaos/vicuna/main/installer.sh
+if [ ! -f /root/installed ]; then
+    wget https://raw.githubusercontent.com/killerkaos/vicuna/main/installer.sh > /proc/1/fd/1
     bash installer.sh
     rm -f installer.sh
 else
@@ -24,7 +15,7 @@ fi
 echo "=========================================" > /proc/1/fd/1
 echo "STARTUP python server script" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-wget https://raw.githubusercontent.com/killerkaos/vicuna/main/startup.sh
+wget https://raw.githubusercontent.com/killerkaos/vicuna/main/startup.sh > /proc/1/fd/1
 bash startup.sh
 
 exec "$@"
