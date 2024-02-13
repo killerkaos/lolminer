e60a07f909878cc3f3f321a7c30874d24c9f38c0
killerkaos <gesthal@gmail.com>
2024-02-13 17:03:57 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-conda init bash > /proc/1/fd/1
-source ~/.bashrc
-
-conda activate /home/abc/vicuna
-
 echo "=========================================" > /proc/1/fd/1
-echo "EXECUTING PYTHON SERVER" > /proc/1/fd/1
+echo "EXECUTING lolminer service" > /proc/1/fd/1
 echo "=========================================" > /proc/1/fd/1
-cd /home/abc/text-generation-webui
-python server.py --listen > /proc/1/fd/1
